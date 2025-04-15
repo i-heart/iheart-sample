7aa1cbe097b4588e82e5a07fc02acbe0f2671d7a
mhpark <minoflower@i-heart.co.kr>
2025-04-15 16:36:53 +0900

    fix: axios-retry 반영

--- mars/nodejs/send.js
+++ mars/nodejs/send.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const axiosRetry = require("axios-retry").default;
 const fs = require("fs");
 const FormData = require("form-data");
 
@@ -10,98 +11,113 @@ const user = {
   password: "dkdlgkxm1!",
 };
 
-(async () => {
-  await sendMessage();
-})();
+axiosRetry(axios, {
+  retries: 1,
+  retryCondition: async (error) => {
+    const code = error.response?.data?.code;
+    let isRetry = ["401", "498"].includes(code);
+
+    if(isRetry) {
+      console.log("⚠️ 토큰이 없거나 만료로 재인증 시도중...", "\n");
+      user.accessToken = "";
+
+      await authenticate();
+
+      error.config.headers['Authorization'] = `Bearer ${user.accessToken}`
+    }
+
+    return isRetry;
+  }
+})
 
 async function sendMessage() {
 
   // 1. SMS 발송
-  await fetchApi(() =>
-      axios.post(
-          `${BASE_URL}/api/v1/send/sms`,
+  await axios.post(
+      `${BASE_URL}/api/v1/send/sms`,
+      {
+        callback: "16442105",
+        message: "안녕하세요. #{회사명} 소속 #{이름}입니다.",
+        receiverList: [
           {
-            callback: "16442105",
-            message: "안녕하세요. #{회사명} 소속 #{이름}입니다.",
-            receiverList: [
-              {
-                phone: "01001231234",
-                userKey: "iheart-sms-1",
-                customFields: {
-                  "이름": "김바른",
-                  "회사명": "아이하트",
-                },
-              },
-            ],
-          },
-          {
-            headers: {
-              "Authorization": `Bearer ${user.accessToken}`,
-              "Content-Type": "application/json; charset=utf-8",
+            phone: "01001231234",
+            userKey: "iheart-sms-1",
+            customFields: {
+              "이름": "김바른",
+              "회사명": "아이하트",
             },
-          }
-      )
-  );
+          },
+        ],
+      },
+      {
+        headers: {
+          "Authorization": `Bearer ${user.accessToken}`,
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+  ).then(response => {
+    console.log(response.data);
+  });
 
   // 2. LMS 발송
-  await fetchApi(() =>
-      axios.post(
-          `${BASE_URL}/api/v1/send/mms`, // MMS API 사용, fileId 없으면 LMS
+  await axios.post(
+      `${BASE_URL}/api/v1/send/mms`, // MMS API 사용, fileId 없으면 LMS
+      {
+        callback: "16442105",
+        subject: "안내드립니다",
+        message: "LMS 메시지입니다. #{이름} 고객님 확인 부탁드립니다.",
+        receiverList: [
           {
-            callback: "16442105",
-            subject: "안내드립니다",
-            message: "LMS 메시지입니다. #{이름} 고객님 확인 부탁드립니다.",
-            receiverList: [
-              {
-                phone: "01001231234",
-                userKey: "iheart-lms-1",
-                customFields: {
-                  "이름": "김찬란",
-                },
-              },
-            ],
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${user.accessToken}`,
-              "Content-Type": "application/json; charset=utf-8",
+            phone: "01001231234",
+            userKey: "iheart-lms-1",
+            customFields: {
+              "이름": "김찬란",
             },
-          }
-      )
-  );
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+  ).then(response => {
+    console.log(response.data);
+  });
 
   // 3. MMS 발송
   // 3-1. fileId 가져오기
-  const {fileId} = await fetchApi(() => uploadFile("MMS"));
+  const {fileId} = await uploadFile("MMS");
 
   // 3-2. MMS 발송 요청
-  await fetchApi(() =>
-      axios.post(
-          `${BASE_URL}/api/v1/send/mms`,
+  await axios.post(
+      `${BASE_URL}/api/v1/send/mms`,
+      {
+        callback: "16442105",
+        subject: "알립니다",
+        message: "#{이름}님의 이메일 주소가 #{이메일}인지 확인해 주세요.",
+        receiverList: [
           {
-            callback: "16442105",
-            subject: "알립니다",
-            message: "#{이름}님의 이메일 주소가 #{이메일}인지 확인해 주세요.",
-            receiverList: [
-              {
-                phone: "01001231234",
-                userKey: "iheart-mms-1",
-                customFields: {
-                  "이름": "김바른",
-                  "이메일": "iheart@i-heart.kr",
-                },
-              },
-            ],
-            fileIdList: [fileId],
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${user.accessToken}`,
-              "Content-Type": "application/json; charset=utf-8",
+            phone: "01001231234",
+            userKey: "iheart-mms-1",
+            customFields: {
+              "이름": "김바른",
+              "이메일": "iheart@i-heart.kr",
             },
-          }
-      )
-  );
+          },
+        ],
+        fileIdList: [fileId],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+  ).then(response => {
+    console.log(response.data);
+  });
 }
 
 async function authenticate() {
@@ -119,31 +135,6 @@ async function authenticate() {
   }
 }
 
-async function fetchApi(apiCallFn) {
-  let attempt = 0;
-
-  while (attempt < 2) {
-    try {
-      const {data} = await apiCallFn();
-      console.log(data, "\n");
-      return data;
-    } catch (err) {
-      const code = err.response?.data.code;
-
-      if (code === "401" || code === "498" || attempt > 0) {
-        console.log("⚠️ 토큰이 없거나 만료로 재인증 후 재시도 중...", "\n");
-        user.accessToken = "";
-
-        await authenticate();
-        attempt++;
-      } else {
-        console.error(`❌ API 호출 실패 - code: ${code || 'unknown'}`);
-        throw err;
-      }
-    }
-  }
-}
-
 async function uploadFile(fileType) {
   const form = new FormData();
   form.append("filePart", fs.createReadStream(FILE_PATH));
@@ -156,5 +147,11 @@ async function uploadFile(fileType) {
     },
   });
 
-  return data;
+  console.log(data);
+
+  return data.data;
 }
+
+(async () => {
+  await sendMessage();
+})();
